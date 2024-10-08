9d7733045cbac803c496dc5739e709a477e77faa
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-10-08 13:03:09 +0530

    Added files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
+
+const Navbar = () => {
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple logo" width={14} height={18} />
+
+        <div className="flex justify-center flex-1 max-sm:hidden">
+          {navLists.map((nav) => {
+            return (
+              <div
+                key={nav}
+                className="px-5 text-sm cursor-pointer text-gray-100 hover:text-white transition-all"
+              >
+                {nav}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="search" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
